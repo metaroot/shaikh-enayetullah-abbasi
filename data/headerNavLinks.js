479addae39890c45a903b47c1ba68872d7a96ebe
metaroot <faisal.ahmed.cse.sust@gmail.com>
2023-01-07 17:14:23 +0600

    first commit

---
+++ data/headerNavLinks.js
@@ -0,0 +1,19 @@
+const headerNavLinks = [
+  {
+    href: '/blog',
+    title: 'আলোচনা',
+  },
+  {
+    href: '/tags',
+    title: 'বিষয়',
+  },
+  {
+    href: '/projects',
+    title: 'প্রকল্প',
+  },
+  {
+    href: '/about',
+    title: 'পরিচিতি',
+  },
+]
+export default headerNavLinks
